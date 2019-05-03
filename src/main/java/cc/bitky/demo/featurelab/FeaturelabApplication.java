5af1af3f42a082bbd10240a36f11622d327a7460
123lml123 <bitkylin@163.com>
2019-05-03 10:13:35 +0800

    初始化

---
+++ src/main/java/cc/bitky/demo/featurelab/FeaturelabApplication.java
@@ -0,0 +1,13 @@
+package cc.bitky.demo.featurelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturelabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeaturelabApplication.class, args);
+    }
+
+}
