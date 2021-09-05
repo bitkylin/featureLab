26fa3d68e8652ce2fbef1bd5025523516468bf2d
bitkylin <bitkylin@163.com>
2021-09-05 15:42:00 +0800

    并发编程实战1,2

---
+++ casper-lab/src/main/java/cc/bitky/featurelab/casperlab/service/java/designpattern/Main.java
@@ -0,0 +1,13 @@
+package cc.bitky.featurelab.casperlab.service.java.designpattern;
+
+/**
+ * @author limingliang
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main");
+        Class clz = Single.class;
+        System.out.println("main");
+        Single single = new Single();
+    }
+}
---
+++ casper-lab/src/main/java/cc/bitky/featurelab/casperlab/service/java/designpattern/Single.java
@@ -0,0 +1,11 @@
+package cc.bitky.featurelab.casperlab.service.java.designpattern;
+
+/**
+ * @author limingliang
+ */
+public class Single {
+
+    static {
+        System.out.println("single");
+    }
+}
