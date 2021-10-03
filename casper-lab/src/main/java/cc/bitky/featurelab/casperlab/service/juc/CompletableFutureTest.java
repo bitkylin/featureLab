20678fbfd146945591a0423ab828d241803e9904
bitkylin <bitkylin@163.com>
2021-10-03 14:40:36 +0800

    并发：27；设计模式：57

--- casper-lab/src/main/java/cc/bitky/featurelab/casperlab/service/juc/CompletableFutureTest.java
+++ casper-lab/src/main/java/cc/bitky/featurelab/casperlab/service/juc/CompletableFutureTest.java
@@ -1,9 +1,8 @@
 package cc.bitky.featurelab.casperlab.service.juc;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.*;
 
 /**
  * @author limingliang
@@ -11,6 +10,35 @@ import java.util.concurrent.TimeoutException;
 public class CompletableFutureTest {
 
     public static void main(String[] args) throws InterruptedException {
+//        simpleRun();
+        CompletableFuture f1 = CompletableFuture.supplyAsync(() -> {
+            int t = getRandom(5, 10);
+            sleep(t);
+            return String.valueOf(t);
+        });
+        CompletableFuture f2 = CompletableFuture.supplyAsync(() -> {
+            int t = getRandom(5, 10);
+            sleep(t);
+            return String.valueOf(t);
+        });
+        CompletableFuture f3 = f1.applyToEither(f2, s -> {
+            return s;
+        });
+        System.out.println(f1.join());
+        System.out.println(f2.join());
+        System.out.println(f3.join());
+    }
+
+    @SneakyThrows
+    private static void sleep(int time) {
+        Thread.sleep(time * 100);
+    }
+
+    private static int getRandom(int origin, int bound) {
+        return ThreadLocalRandom.current().nextInt(origin, bound);
+    }
+
+    private static void simpleRun() throws InterruptedException {
         try {
             CompletableFuture.runAsync(() -> {
                 try {
