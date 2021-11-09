988ca5c912678d35d531db48b63a1c285151c307
bitkylin <bitkylin@163.com>
2021-11-09 10:22:21 +0800

    i++练习

--- bitkylin-algorithm/section-8/leetcode8/IPlusPlus.java
+++ bitkylin-algorithm/section-8/leetcode8/IPlusPlus.java
@@ -9,52 +9,71 @@ public class IPlusPlus {
         int i = 0, j = 0, k = 0;
 
         // 响应 3 = 0 + 1 + 2
-        System.out.println(test(i++, i++, i++));
+        log("test(i++, i++, i++)");
+        log(test(i++, i++, i++));
 
         i = 0;
         j = 0;
         k = 0;
         // 入参：0, 0, 0
-        System.out.println(test(i++, j++, k++));
+        log("test(i++, j++, k++)");
+        log(test(i++, j++, k++));
 
         i = 0;
         j = 0;
         k = 0;
         // 响应：0
-        System.out.println((i++ + j++ + k++));
+        log("i++ + j++ + k++");
+        log(i++ + j++ + k++);
 
         i = 0;
         j = 0;
         k = 0;
         // 响应：1 = 0 + 1 + 0
-        System.out.println((j++ + j++ + k++));
+        log("j++ + j++ + k++");
+        log(j++ + j++ + k++);
 
         i = 0;
         j = 0;
         k = 0;
         // 响应：0
-        System.out.println(test(i++) + test(j++) + test(k++));
+        log("test(i++) + test(j++) + test(k++)");
+        log(test(i++) + test(j++) + test(k++));
+
+
+        i = 0;
+        j = 0;
+        k = 0;
+        // 响应：1 = 0 + 0 + 1
+        log("test(i++) + test(j++) + test(i++)");
+        log(test(i++) + test(j++) + test(i++));
 
 
         i = 0;
         j = 0;
         k = 0;
         // 响应：7 = 0 + 1 + 2 + 4 + 0 + 0
-        System.out.println(i++ + i++ + i++ + ++i + j++ + k++);
+        log("i++ + i++ + i++ + ++i + j++ + k++");
+        log(i++ + i++ + i++ + ++i + j++ + k++);
 
         i = 0;
         j = 0;
         k = 0;
         j = 1 > i++ ? i++ : i++;
         // 响应：1:2
-        System.out.println(j + ":" + i);
+        log("j + : + i");
+        log(j + ":" + i);
     }
 
-    public static int test(int i, int j, int k) {
+    private static void log(Object msg) {
+        System.out.println(msg);
+    }
+
+    private static int test(int i, int j, int k) {
         return i + j + k;
     }
 
-    public static int test(int i) {
+    private static int test(int i) {
         return i;
     }
 }
