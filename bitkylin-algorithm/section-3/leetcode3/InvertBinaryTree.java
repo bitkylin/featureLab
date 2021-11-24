3f560bca05b14ce29502b9885185502f3301f3fd
bitkylin <bitkylin@163.com>
2021-11-24 18:39:42 +0800

    section 3 - 226. 翻转二叉树；98. 验证二叉搜索树

--- bitkylin-algorithm/section-3/leetcode3/InvertBinaryTree.java
+++ bitkylin-algorithm/section-3/leetcode3/InvertBinaryTree.java
@@ -1,35 +1,35 @@
-//翻转一棵二叉树。 
-//
-// 示例： 
-//
-// 输入： 
-//
-//      4
-//   /   \
-//  2     7
-// / \   / \
-//1   3 6   9 
-//
-// 输出： 
-//
-//      4
-//   /   \
-//  7     2
-// / \   / \
-//9   6 3   1 
-//
-// 备注: 
-//这个问题是受到 Max Howell 的 原问题 启发的 ： 
-//
-// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。 
-// Related Topics 树 
-// 👍 631 👎 0
-
+/**
+ * <p>翻转一棵二叉树。</p>
+ *
+ * <p><strong>示例：</strong></p>
+ *
+ * <p>输入：</p>
+ *
+ * <pre>     4
+ * /   \
+ * 2     7
+ * / \   / \
+ * 1   3 6   9</pre>
+ *
+ * <p>输出：</p>
+ *
+ * <pre>     4
+ * /   \
+ * 7     2
+ * / \   / \
+ * 9   6 3   1</pre>
+ *
+ * <p><strong>备注:</strong><br>
+ * 这个问题是受到 <a href="https://twitter.com/mxcl" target="_blank">Max Howell </a>的 <a href="https://twitter.com/mxcl/status/608682016205344768" target="_blank">原问题</a> 启发的 ：</p>
+ *
+ * <blockquote>谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。</blockquote>
+ * <div><div>Related Topics</div><div><li>树</li><li>深度优先搜索</li><li>广度优先搜索</li><li>二叉树</li></div></div><br><div><li>👍 1071</li><li>👎 0</li></div>
+ */
 
 package leetcode3;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 
 public class InvertBinaryTree {
 
@@ -42,90 +42,76 @@ public class InvertBinaryTree {
         TreeNode left;
         TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
 
-//leetcode submit region begin(Prohibit modification and deletion)
+    //leetcode submit region begin(Prohibit modification and deletion)
 
     /**
-     * 直接套用递归模板即可，很简单
+     * 「递归」时间复杂度：O(n)
      */
     class Solution {
         public TreeNode invertTree(TreeNode root) {
-            if (root == null) {
-                return null;
-            }
-
-            TreeNode k = root.left;
+            if (root == null) return null;
+            TreeNode temp = root.left;
             root.left = root.right;
-            root.right = k;
+            root.right = temp;
             invertTree(root.left);
             invertTree(root.right);
             return root;
         }
     }
-
 //leetcode submit region end(Prohibit modification and deletion)
 
     /**
-     * DFS「深度优先遍历」使用栈
+     * 「循环」「DFS『深度优先遍历』」时间复杂度：O(n)
      */
     class Solution2 {
         public TreeNode invertTree(TreeNode root) {
-            if (root == null) {
-                return null;
-            }
-
-            Deque<TreeNode> deque = new LinkedList<>();
-            deque.addLast(root);
-
-            while (!deque.isEmpty()) {
-                TreeNode node = deque.removeLast();
-                if (node.right != null) {
-                    deque.addLast(node.right);
-                }
-                if (node.left != null) {
-                    deque.addLast(node.left);
-                }
-                TreeNode k = node.left;
+            if (root == null) return null;
+            Deque<TreeNode> stack = new ArrayDeque<>();
+            stack.push(root);
+            while (!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                if (node.left != null) stack.push(node.left);
+                if (node.right != null) stack.push(node.right);
+                TreeNode temp = node.left;
                 node.left = node.right;
-                node.right = k;
+                node.right = temp;
             }
-
             return root;
         }
     }
 
     /**
-     * 层序遍历，BFS「广度优先遍历」使用队列
+     * 「循环」「BFS『广度优先遍历』『层序遍历』」时间复杂度：O(n)
      */
     class Solution3 {
         public TreeNode invertTree(TreeNode root) {
-            if (root == null) {
-                return null;
-            }
-
-            Deque<TreeNode> deque = new LinkedList<>();
-            deque.addLast(root);
-
+            if (root == null) return null;
+            Deque<TreeNode> deque = new ArrayDeque<>();
+            deque.offer(root);
             while (!deque.isEmpty()) {
-                TreeNode node = deque.removeFirst();
-                if (node.left != null) {
-                    deque.addLast(node.left);
-                }
-                if (node.right != null) {
-                    deque.addLast(node.right);
-                }
-                TreeNode k = node.left;
+                TreeNode node = deque.poll();
+                if (node.left != null) deque.push(node.left);
+                if (node.right != null) deque.push(node.right);
+                TreeNode temp = node.left;
                 node.left = node.right;
-                node.right = k;
+                node.right = temp;
             }
-
             return root;
         }
     }
-
 
 }
--- bitkylin-algorithm/section-3/leetcode3/ValidateBinarySearchTree.java
+++ bitkylin-algorithm/section-3/leetcode3/ValidateBinarySearchTree.java
@@ -1,89 +1,90 @@
-//给定一个二叉树，判断其是否是一个有效的二叉搜索树。
-//
-// 假设一个二叉搜索树具有如下特征： 
-//
-// 
-// 节点的左子树只包含小于当前节点的数。 
-// 节点的右子树只包含大于当前节点的数。 
-// 所有左子树和右子树自身必须也是二叉搜索树。 
-// 
-//
-// 示例 1: 
-//
-// 输入:
-//    2
-//   / \
-//  1   3
-//输出: true
-// 
-//
-// 示例 2: 
-//
-// 输入:
-//    5
-//   / \
-//  1   4
-//     / \
-//    3   6
-//输出: false
-//解释: 输入为: [5,1,4,null,null,3,6]。
-//     根节点的值为 5 ，但是其右子节点值为 4 。
-// 
-// Related Topics 树 深度优先搜索 
-// 👍 772 👎 0
-
+/**
+ * <p>给你一个二叉树的根节点 <code>root</code> ，判断其是否是一个有效的二叉搜索树。</p>
+ *
+ * <p><strong>有效</strong> 二叉搜索树定义如下：</p>
+ *
+ * <ul>
+ * <li>节点的左子树只包含<strong> 小于 </strong>当前节点的数。</li>
+ * <li>节点的右子树只包含 <strong>大于</strong> 当前节点的数。</li>
+ * <li>所有左子树和右子树自身必须也是二叉搜索树。</li>
+ * </ul>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>示例 1：</strong></p>
+ * <img alt="" src="https://assets.leetcode.com/uploads/2020/12/01/tree1.jpg" style="width: 302px; height: 182px;" />
+ * <pre>
+ * <strong>输入：</strong>root = [2,1,3]
+ * <strong>输出：</strong>true
+ * </pre>
+ *
+ * <p><strong>示例 2：</strong></p>
+ * <img alt="" src="https://assets.leetcode.com/uploads/2020/12/01/tree2.jpg" style="width: 422px; height: 292px;" />
+ * <pre>
+ * <strong>输入：</strong>root = [5,1,4,null,null,3,6]
+ * <strong>输出：</strong>false
+ * <strong>解释：</strong>根节点的值是 5 ，但是右子节点的值是 4 。
+ * </pre>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>提示：</strong></p>
+ *
+ * <ul>
+ * <li>树中节点数目范围在<code>[1, 10<sup>4</sup>]</code> 内</li>
+ * <li><code>-2<sup>31</sup> &lt;= Node.val &lt;= 2<sup>31</sup> - 1</code></li>
+ * </ul>
+ * <div><div>Related Topics</div><div><li>树</li><li>深度优先搜索</li><li>二叉搜索树</li><li>二叉树</li></div></div><br><div><li>👍 1313</li><li>👎 0</li></div>
+ */
 
 package leetcode3;
 
 public class ValidateBinarySearchTree {
 
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        node2.left = node1;
-        node2.right = node3;
-
-        new ValidateBinarySearchTree().new Solution().isValidBST(node2);
+        Solution solution = new ValidateBinarySearchTree().new Solution();
     }
 
-    public static class TreeNode {
+    public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
 
-//leetcode submit region begin(Prohibit modification and deletion)
+    //leetcode submit region begin(Prohibit modification and deletion)
 
     /**
-     * 二叉搜索树的中序遍历是单调递增的，直接使用中序遍历验证即可
+     * 「递归」二叉搜索树的中序遍历是单调递增的，直接使用中序遍历验证即可
+     * 注：不可仅将节点值和其左右节点比较
+     * 注：当前节点应大于左子树的所有节点，小于右子树的所有节点
+     * 注；递归终止条件，处理当前层，递归下一层
+     * 时间复杂度：O(n)
      */
     class Solution {
-        Integer min = null;
+
+        Integer min;
 
         public boolean isValidBST(TreeNode root) {
-            return resolve(root);
-        }
-
-        private boolean resolve(TreeNode root) {
-            if (root == null) {
-                return true;
-            }
-            if (!resolve(root.left)) {
-                return false;
-            }
-            if (min != null && min >= root.val) {
-                return false;
-            }
+            if (root == null) return true;
+            if (!isValidBST(root.left)) return false;
+            if (min != null && root.val <= min) return false;
             min = root.val;
-            return resolve(root.right);
+            return isValidBST(root.right);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
-
 
 }
