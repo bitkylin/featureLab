2e54a8f7d10ffefd31a777d4a817693dc02056e7
bitkylin <bitkylin@163.com>
2022-01-26 14:20:38 +0800

    section - 9 - first

--- bitkylin-algorithm/section-9/leetcode9/FirstUniqueCharacterInAString.java
+++ bitkylin-algorithm/section-9/leetcode9/FirstUniqueCharacterInAString.java
@@ -1,22 +1,22 @@
-//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。 
-//
-// 
-//
-// 示例： 
-//
-// s = "leetcode"
-//返回 0
-//
-//s = "loveleetcode"
-//返回 2
-// 
-//
-// 
-//
-// 提示：你可以假定该字符串只包含小写字母。 
-// Related Topics 哈希表 字符串 
-// 👍 288 👎 0
-
+/**
+ * <p>给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。</p>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>示例：</strong></p>
+ *
+ * <pre>s = &quot;leetcode&quot;
+ * 返回 0
+ *
+ * s = &quot;loveleetcode&quot;
+ * 返回 2
+ * </pre>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>提示：</strong>你可以假定该字符串只包含小写字母。</p>
+ * <div><div>Related Topics</div><div><li>队列</li><li>哈希表</li><li>字符串</li><li>计数</li></div></div><br><div><li>👍 497</li><li>👎 0</li></div>
+ */
 
 package leetcode9;
 
@@ -26,8 +26,22 @@ public class FirstUniqueCharacterInAString {
         Solution solution = new FirstUniqueCharacterInAString().new Solution();
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        public int firstUniqChar(String s) {
+            int[] arr = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                arr[s.charAt(i) - 0x61]++;
+            }
+            for (int i = 0; i < s.length(); i++) {
+                if (arr[s.charAt(i) - 0x61] == 1) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    class Solution2 {
         public int firstUniqChar(String s) {
             if (s == null || s.length() == 0) {
                 return -1;
@@ -45,7 +59,4 @@ public class FirstUniqueCharacterInAString {
             return -1;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
-
-
 }
