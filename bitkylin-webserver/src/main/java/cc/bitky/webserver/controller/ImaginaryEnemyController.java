68dc73fd1415c21982f97cf7d6c7eb4ec39e8293
bitkylin <bitkylin@163.com>
2023-10-05 12:36:02 +0800

    imaginary enemy controller

--- bitkylin-webserver/src/main/java/cc/bitky/webserver/controller/ImaginaryEnemyController.java
+++ bitkylin-webserver/src/main/java/cc/bitky/webserver/controller/ImaginaryEnemyController.java
@@ -14,17 +14,36 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
+ * 假想敌服务
+ *
  * @author limingliang
  */
 @Controller
 @RestController
 @Slf4j
-public class FileController {
+public class ImaginaryEnemyController {
 
-    @GetMapping("/download/" + "**")
-    public ResponseEntity<Resource> downloadFile(@RequestParam String path) {
+    /**
+     * 元素刺探
+     */
+    @GetMapping("/spyElement/" + "**")
+    public ResponseEntity<Resource> spyElement(@RequestParam String path) {
+        Path filePath = Paths.get(path);
+        return createResourceResponseEntity(filePath);
+    }
+
+    @GetMapping("/spy/doc")
+    public ResponseEntity<Resource> spyDoc() {
+        return createResourceResponseEntity(Paths.get("/Users/lml/Documents/backup/doc"));
+    }
+
+    @GetMapping("/spy/dev")
+    public ResponseEntity<Resource> spyDev() {
+        return createResourceResponseEntity(Paths.get("/Users/lml/Documents/backup/dev"));
+    }
+
+    private ResponseEntity<Resource> createResourceResponseEntity(Path filePath) {
         try {
-            Path filePath = Paths.get(path);
             HttpHeaders headers = new HttpHeaders();
             headers.setContentDisposition(ContentDisposition.attachment()
                     .filename(filePath.getFileName().toString(), StandardCharsets.UTF_8)
