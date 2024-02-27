24f577474740712fca7db4b2d9bd2353fed017bc
bitkylin <bitkylin@163.com>
2024-02-27 11:38:44 +0800

    imaginary enemy controller

--- bitkylin-webserver/src/main/java/cc/bitky/webserver/WebserverApplication.java
+++ bitkylin-webserver/src/main/java/cc/bitky/webserver/WebserverApplication.java
@@ -1,13 +1,15 @@
 package cc.bitky.webserver;
 
+import cc.bitky.webserver.controller.ImaginaryEnemyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ImaginaryEnemyController.setAlley(args[0]);
+        SpringApplication.run(WebserverApplication.class, args);
+    }
 
 }
--- bitkylin-webserver/src/main/java/cc/bitky/webserver/controller/ImaginaryEnemyController.java
+++ bitkylin-webserver/src/main/java/cc/bitky/webserver/controller/ImaginaryEnemyController.java
@@ -1,5 +1,6 @@
 package cc.bitky.webserver.controller;
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.function.Supplier;
 
 /**
  * 假想敌服务
@@ -24,22 +27,59 @@ import java.nio.file.Paths;
 public class ImaginaryEnemyController {
 
     /**
-     * 元素刺探
+     * 胡同
      */
-    @GetMapping("/spyElement/" + "**")
-    public ResponseEntity<Resource> spyElement(@RequestParam String path) {
-        Path filePath = Paths.get(path);
-        return createResourceResponseEntity(filePath);
+    @Setter
+    private static String alley = "";
+
+    /**
+     * 伙计刺探
+     */
+    @GetMapping("/spyGuy/" + "**")
+    public ResponseEntity<Resource> spyElement(@RequestParam String guy) {
+        return run(() -> createResourceResponseEntity(searchGuy(alley, guy)));
     }
 
+    /**
+     * 刺探医生
+     */
     @GetMapping("/spy/doc")
     public ResponseEntity<Resource> spyDoc() {
-        return createResourceResponseEntity(Paths.get("/Users/lml/Documents/backup/doc"));
+        return run(() -> createResourceResponseEntity(searchGuy(alley, "doc")));
     }
 
+    /**
+     * 刺探德芙
+     */
     @GetMapping("/spy/dev")
     public ResponseEntity<Resource> spyDev() {
-        return createResourceResponseEntity(Paths.get("/Users/lml/Documents/backup/dev"));
+        return run(() -> createResourceResponseEntity(searchGuy(alley, "dev")));
+    }
+
+    /**
+     * 查找指定的伙计
+     */
+    private Path searchGuy(String alley, String guy) {
+        Path houseNumber = Paths.get(alley, guy);
+        File house = houseNumber.toFile();
+        if (!house.exists()) {
+            throw new RuntimeException("伙计不在家:" + houseNumber);
+        }
+        if (!house.canRead()) {
+            throw new RuntimeException("伙计拒绝见面:" + houseNumber);
+        }
+        return houseNumber;
+    }
+
+    private ResponseEntity run(Supplier<ResponseEntity> runnable) {
+        try {
+            return runnable.get();
+        } catch (Exception e) {
+            return ResponseEntity.status(400)
+                    .contentType(MediaType.parseMediaType("text/plain; charset=utf-8"))
+                    .body(e.getMessage());
+        }
+
     }
 
     private ResponseEntity<Resource> createResourceResponseEntity(Path filePath) {
